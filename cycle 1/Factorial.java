681c9bd4357bf2590f07dc1132993b7a3aa359f1
jeethumelel <98574524+jeethumelel@users.noreply.github.com>
2022-01-28 10:16:47 +0400

    Create Factorial.java

---
+++ cycle 1/Factorial.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Factorial 
+{
+    public static void main(String args [])
+    {
+        int i,fact=1;
+        Scanner obj=new Scanner(System.in);
+        System.out.println("Enter the number ");
+        int n=obj.nextInt();
+      
+        for(i=1; i<=n; i++)
+            fact=fact*i;
+        System.out.println("The factorial of "+n+" is "+fact);    
+
+    }
+}
